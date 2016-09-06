9e3881fa5cd415d71e2de3714d7a2becb9264e0f
Noel Walsh <noel@crewe-it.co.uk>
2016-09-06 19:36:58 +0100

    Email with total new keys listed at the top

--- console/mail/stockItemCreatedEmail.php
+++ console/mail/stockItemCreatedEmail.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 use common\models\Account;
 
 /**
- * @var string $subject
- * @var \amnah\yii2\user\models\User $user
+ * @var string                          $subject
+ * @var \amnah\yii2\user\models\User    $user
  * @var \amnah\yii2\user\models\Profile $profile
  * @var \amnah\yii2\user\models\UserKey $userKey
  *
@@ -14,61 +14,153 @@ use common\models\Account;
  *
  */
 
-$data = $selectedDetails ;
+$showKeys  = isset($selectedDetails['showkeys']) && $selectedDetails['showkeys'];
+$totalKeys = 0;
+foreach ($selectedDetails['pos'] as $purchaseOrderNumber => $po) {
+    $totalKeys += count($po);
+}
 
+$totalKeysMessage = Yii::t('app', $totalKeys . ' {totalkeys, plural, =0{No new keys} =1{New key is} other{New keys are}} now available in', ['totalkeys' => $totalKeys]) ;
 ?>
 
-<style>body {background-color:#f2f2f2;} body * {font-family: verdana;font-size: 8pt;} .email-body {width: 100%;padding: 0;margin:0;text-align:center;background-color:#f2f2f2;font-family: verdana;}.email-body th {font-family: verdana;font-size: 8pt;}.email-body td {font-family: verdana;font-size: 8pt;}.email-body h2 {font-family: verdana;font-size: 8pt;}.email-body h3 {font-family: verdana;font-size: 8pt;}.email-body h4 {font-family: verdana;font-size: 8pt;}.email-header {width:650px;height:132px;}.email-border {width:20px;}.email-content {font-family: verdana;padding: 20px;background-color:#ffffff;text-align: left;}.email-pre-footer {width:650px;height:18pt;}.email-footer {width: 650px;height:24px;}
-.stockitem {border:1px dotted black; padding: 10px; margin-top:10px;} .boxshot {width:100px;}
-.stockitem tbody tr td {border-bottom: 1px solid lightgray; padding-bottom: 10px; padding-top: 10px;}
-.accountlogo {width:100px; padding:20px;}
-.vendor-logo {height:60px;}
-</style>
+<style>body {
+        background-color: #f2f2f2;
+    }
 
+    body * {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-body {
+        width: 100%;
+        padding: 0;
+        margin: 0;
+        text-align: center;
+        background-color: #f2f2f2;
+        font-family: verdana;
+    }
+
+    .email-body th {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-body td {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-body h2 {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-body h3 {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-body h4 {
+        font-family: verdana;
+        font-size: 8pt;
+    }
+
+    .email-header {
+        width: 650px;
+        height: 132px;
+    }
+
+    .email-border {
+        width: 20px;
+    }
+
+    .email-content {
+        font-family: verdana;
+        padding: 20px;
+        background-color: #ffffff;
+        text-align: left;
+    }
+
+    .email-pre-footer {
+        width: 650px;
+        height: 18pt;
+    }
+
+    .email-footer {
+        width: 650px;
+        height: 24px;
+    }
+
+    .stockitem {
+        border: 1px dotted black;
+        padding: 10px;
+        margin-top: 10px;
+    }
+
+    .boxshot {
+        width: 100px;
+    }
+
+    .stockitem tbody tr td {
+        border-bottom: 1px solid lightgray;
+        padding-bottom: 10px;
+        padding-top: 10px;
+    }
+
+    .accountlogo {
+        width: 100px;
+        padding: 20px;
+    }
+
+    .vendor-logo {
+        height: 60px;
+    }
+</style>
 
 
 <TABLE border=0 cellSpacing=0 cellPadding=0 width=650>
     <TBODY>
-        <TR>
-            <TD class="email-header" colSpan=3><IMG
+    <TR>
+        <TD class="email-header" colSpan=3><IMG
                     src="http://email.exertismicro-p.biz/img/simple-header-white.jpg"
                     NOSEND="1"></TD>
-        </TR>
-        <tr>
+    </TR>
+    <tr>
+        <?php
+        if (!empty($account->accountLogo)) {
+            echo '<td>' . Html::img($account->accountLogo, ['style' => 'width:100px; padding:20px;']) . '</td>';
+        }
+        ?>
+        <td colSpan=2>
+
+            <h3><?= $account->customer_exertis_account_number ?></h3>
+            <p><?= $account->findMainUser()->email ?></p>
+
+            <p><?= $totalKeysMessage ?>
+                your <?= Html::a('Exertis Digital Stock Room', 'https://stockroom.exertis.co.uk') ?>.</p>
+            <p>Please visit <?= Html::a('our Help Page', 'https://stockroom.exertis.co.uk/site/help') ?> for FAQs,
+                Support Contacts and Download Links</p>
+        </td>
+    </tr>
+    <TR>
+        <!--<TD class="email-border" width=20></TD>-->
+        <TD class="email-content" width=810 colspan="3">
+
             <?php
-                if (!empty($account->accountLogo)) {
-                    echo '<td>'.Html::img($account->accountLogo, ['style'=>'width:100px; padding:20px;']).'</td>';
-                }
-            ?>
-            <td colSpan=2>
+            foreach ($selectedDetails['pos'] as $purchaseOrderNumber => $po) {
+                foreach ($po as $dataitem) { ?>
 
-                <h3><?= $account->customer_exertis_account_number ?></h3>
-                <p><?= $account->findMainUser()->email ?></p>
-
-                <p>< ?=count($dataitem)?> New keys are now available in your <?= Html::a('Exertis Digital Stock Room', 'https://stockroom.exertis.co.uk') ?>.</p>
-                <p>Please visit <?= Html::a('our Help Page', 'https://stockroom.exertis.co.uk/site/help') ?> for FAQs, Support Contacts and Download Links</p>
-            </td>
-        </tr>
-        <TR>
-            <!--<TD class="email-border" width=20></TD>-->
-            <TD class="email-content" width=810 colspan="3">
-
-                  <?php
-                                    foreach($data['pos'] as $key=>$po){
-                                        foreach($po as $dataitem){
-//print_r($dataitem,true);
-                                    ?>
-
-                <TABLE border=0 cellSpacing=0 cellPadding=0 class="stockitem">
-                    <thead>
+                    <TABLE border=0 cellSpacing=0 cellPadding=0 class="stockitem">
+                        <thead>
                         <TR>
                             <th></th>
                             <th>Your PO</th>
                             <th>Our Order</th>
                             <th>Our Partcode</th>
                         </TR>
-                    </thead>
-                    <TBODY>
+                        </thead>
+                        <TBODY>
 
 
                         <TR>
@@ -76,13 +168,13 @@ $data = $selectedDetails ;
                                 <?php
                                 /** @var StockItem $stockItem */
                                 $stockItem = $dataitem['item'];
-                                if ($stockItem && $stockItem->getImageUrl() ) {
-                                    echo Html::img($stockItem->getImageUrl()->image_url, ['class'=>'boxshot']);
-                                    }
+                                if ($stockItem && $stockItem->getImageUrl()) {
+                                    echo Html::img($stockItem->getImageUrl()->image_url, ['class' => 'boxshot']);
+                                }
                                 ?>
                             </td>
-                            <td><?= $key ?></td>
-                            <td><?= $dataitem['orderdetails']->sop.'-'.$stockItem->id.'-'.$stockItem->eztorm_order_id; ?></td>
+                            <td><?= $purchaseOrderNumber ?></td>
+                            <td><?= $dataitem['orderdetails']->sop . '-' . $stockItem->id . '-' . $stockItem->eztorm_order_id; ?></td>
                             <td><?= $dataitem['product']->partcode; ?></td>
                         </TR>
 
@@ -93,24 +185,26 @@ $data = $selectedDetails ;
                             <td colspan="3"><?= $dataitem['orderdetails']->name; ?></td>
                         </TR>
 
-                         <?php if (isset($data['showkeys']) && $data['showkeys']) {
-                         echo '<TR>';
+                        <?php if ($showKeys) {
+                            echo '<TR>';
                             echo '<td colspan="3">';
                             $stockItem = $dataitem['item'];
                             echo common\components\DigitalPurchaser::getProductInstallKey($stockItem);
                             echo '</td>';
-                         echo '</TR>';
-                         } ?>
+                            echo '</TR>';
+                        } ?>
 
-                </TABLE>
-                 <?php  } // foreach pos
-                                          } // foreach po
-                                ?>
-            </TD></TR>
-                <tr>
-                    <?= Html::img(Yii::$app->params['frontendBaseUrl'].'img/microsoft-partner.png', ['class'=>'vendor-logo']); ?>
-                <td>
-            </td></tr>
+                    </TABLE>
+                <?php } // foreach pos
+            } // foreach po
+            ?>
+        </TD>
+    </TR>
+    <tr>
+        <?= Html::img(Yii::$app->params['frontendBaseUrl'] . 'img/microsoft-partner.png', ['class' => 'vendor-logo']); ?>
+        <td>
+        </td>
+    </tr>
     </TBODY>
 </TABLE>
 

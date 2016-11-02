20f6aeb6d9b742f345080da5213918b28f07aafe
Noel Walsh <noel@crewe-it.co.uk>
2016-11-02 19:36:56 +0000

    Added model for new table

---
+++ common/models/CustomerProductMapping.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "customer_product_mapping".
+ *
+ * @property integer $id
+ * @property string $customer_account_number
+ * @property string $customer_partcode
+ * @property string $oracle_partcode
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class CustomerProductMapping extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'customer_product_mapping';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['customer_account_number', 'customer_partcode', 'oracle_partcode', 'created_at', 'updated_at'], 'required'],
+            [['created_at', 'updated_at'], 'integer'],
+            [['customer_account_number'], 'string', 'max' => 20],
+            [['customer_partcode', 'oracle_partcode'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'customer_account_number' => 'Customer Account Number',
+            'customer_partcode' => 'Customer Partcode',
+            'oracle_partcode' => 'Oracle Partcode',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}

4b20ea3515f82468ed1953b5b0349518596e7486
Noel Walsh <noel@crewe-it.co.uk>
2016-11-03 10:37:53 +0000

    New migration and model to hold the dropship order details

---
+++ common/models/DropshipOrderline.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "dropship_orderline".
+ *
+ * @property integer $id
+ * @property integer $dropship_id
+ * @property string $customer_partcode
+ * @property string $oracle_partcode
+ * @property string $quantity
+ * @property double $price
+ * @property integer $created_at
+ * @property integer $updated_at
+ */
+class DropshipOrderline extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'dropship_orderline';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['dropship_id', 'customer_partcode', 'oracle_partcode', 'quantity', 'price', 'created_at', 'updated_at'], 'required'],
+            [['dropship_id', 'created_at', 'updated_at'], 'integer'],
+            [['price'], 'number'],
+            [['customer_partcode', 'oracle_partcode', 'quantity'], 'string', 'max' => 255],
+            [['dropship_id'], 'exist', 'skipOnError' => true, 'targetClass' => DropshipEmailDetails::className(), 'targetAttribute' => ['dropship_id' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'dropship_id' => 'Dropship ID',
+            'customer_partcode' => 'Customer Partcode',
+            'oracle_partcode' => 'Oracle Partcode',
+            'quantity' => 'Quantity',
+            'price' => 'Price',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
---
+++ console/migrations/m161103_101551_add_parcode_quantity_price_to_dropship.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m161103_101551_add_parcode_quantity_price_to_dropship extends Migration {
+    public function up() {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET latin1 COLLATE latin1_swedish_ci ENGINE=INNODB';
+        }
+
+        $this->createTable('{{%dropship_orderline}}', [
+            'id'                => Schema::TYPE_PK,
+            'dropship_id'       => Schema::TYPE_INTEGER . ' NOT NULL COMMENT"This is the related dropship_email_details_record"',
+            'customer_partcode' => Schema::TYPE_STRING . ' NOT NULL COMMENT"The customer\'s own partcode"',
+            'oracle_partcode'   => Schema::TYPE_STRING . ' NOT NULL COMMENT"The oracle version of the customer partcode"',
+            'quantity'          => Schema::TYPE_STRING . ' NOT NULL COMMENT"The number ordered"',
+            'price'             => Schema::TYPE_DOUBLE . ' NOT NULL COMMENT"The order price, which can be zero"',
+
+            'created_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
+            'updated_at'        => Schema::TYPE_INTEGER . ' NOT NULL',
+        ], $tableOptions);
+
+        $this->addForeignKey('ds_email_fk',
+                             '{{%dropship_orderline}}', 'dropship_id',
+                             '{{%dropship_email_details}}', 'id');
+    }
+
+    public function down() {
+        $this->dropTable('{{%dropship_orderline}}');
+    }
+}

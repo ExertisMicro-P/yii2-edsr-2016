3c697c2c2b16269707ff2281616d8cff125ad841
Noel Walsh <noel@crewe-it.co.uk>
2016-11-04 15:12:56 +0000

    Made created_at and updated_at optional (as auto filled on save), then added deleted fields

--- common/models/DropshipOrderline.php
+++ common/models/DropshipOrderline.php
@@ -32,8 +32,8 @@ class DropshipOrderline extends \common\models\BaseModel
     public function rules()
     {
         return [
-            [['dropship_id', 'customer_partcode', 'oracle_partcode', 'quantity', 'price', 'created_at', 'updated_at'], 'required'],
-            [['dropship_id', 'created_at', 'updated_at'], 'integer'],
+            [['dropship_id', 'customer_partcode', 'oracle_partcode', 'quantity', 'price'], 'required'],
+            [['dropship_id', 'created_by', 'updated_by', 'deleted_by'], 'integer'],
             [['price'], 'number'],
             [['customer_partcode', 'oracle_partcode', 'quantity'], 'string', 'max' => 255],
             [['dropship_id'], 'exist', 'skipOnError' => true, 'targetClass' => DropshipEmailDetails::className(), 'targetAttribute' => ['dropship_id' => 'id']],
@@ -53,7 +53,22 @@ class DropshipOrderline extends \common\models\BaseModel
             'quantity' => 'Quantity',
             'price' => 'Price',
             'created_at' => 'Created At',
+            'created_by' => 'Created By',
             'updated_at' => 'Updated At',
+            'updated_by' => 'Updated By',
+            'deleted_at' => 'Deleted At',
+            'deleted_by' => 'Deleted By',
         ];
     }
+
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDropshipOrderlines()
+    {
+        return $this->hasMany(DropshipOrderline::className(), ['dropship_id' => 'id']);
+    }
+
+
 }
